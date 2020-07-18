22c81b42ae9ae1a2ed6ca3fb4cbf6aacbc578c40
Ivan Kahl <hello@ivankahl.com>
2020-07-18 21:59:23 +0200

    Fixed HTTPs check

--- functions/getQuotes.js
+++ functions/getQuotes.js
@@ -1,9 +1,14 @@
 const axios = require("axios");
+const https = require("https");
 
 exports.handler = async event => {
-  const quotes = (await axios.get(
-    "https://quotes.stormconsultancy.co.uk/random.json"
-  )).data;
+  const quotes = (await axios.request({
+    url: "https://quotes.stormconsultancy.co.uk/random.json",
+    method: "POST",
+    httpsAgent: https.Agent({
+      rejectUnauthorized: false
+    })
+  })).data;
 
   return {
     statusCode: 200,
