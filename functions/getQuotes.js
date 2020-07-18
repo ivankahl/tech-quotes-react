bfa0b456f70d0f025883d9f21d825649706f6fe5
Ivan Kahl <hello@ivankahl.com>
2020-07-18 22:01:07 +0200

    Added stringify JSON

--- functions/getQuotes.js
+++ functions/getQuotes.js
@@ -2,16 +2,18 @@ const axios = require("axios");
 const https = require("https");
 
 exports.handler = async event => {
-  const quotes = (await axios.request({
-    url: "https://quotes.stormconsultancy.co.uk/random.json",
-    method: "POST",
-    httpsAgent: https.Agent({
-      rejectUnauthorized: false
-    })
-  })).data;
+  const quotes = await axios.get(
+    "https://quotes.stormconsultancy.co.uk/random.json",
+    {
+      method: "POST",
+      httpsAgent: https.Agent({
+        rejectUnauthorized: false
+      })
+    }
+  );
 
   return {
     statusCode: 200,
-    body: quotes
+    body: JSON.stringify(quotes)
   };
 };
